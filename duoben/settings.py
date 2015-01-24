12245083345c2b403dbd72b7c757b33b9cb51834
xmy <xuemy268@gmail.com>
2015-01-24 21:10:41 +0800

    修改一下setting设置

--- duoben/settings.py
+++ duoben/settings.py
@@ -87,7 +87,12 @@ TEMPLATE_CONTEXT_PROCESSORS += ('django.core.context_processors.request',"app.pr
 
 
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+}
 
 
 from local_settings import *
